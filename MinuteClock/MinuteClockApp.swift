a0a91ef82f8e789e987377a87f8f8e886640291f
Michael Olson <mao@olsons.net>
2024-05-17 07:07:30 -0700

    Initial Commit

---
+++ MinuteClock/MinuteClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinuteClockApp.swift
+//  MinuteClock
+//
+//  Created by Mike Olson on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MinuteClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
